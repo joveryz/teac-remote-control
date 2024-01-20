73b8f192215d46fb39a3c8870122f7fa6fdfa960
Avery Zhang <ztb5129@live.com>
2024-01-20 14:54:11 +0800

    Change server port

--- RemoteControlServer/server.py
+++ RemoteControlServer/server.py
@@ -13,11 +13,11 @@ app.add_middleware(
 )
 
 com_server_host = "172.16.68.188"
-cd_com_port = 32
+cd_com_port = 23
 cd_com_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cd_com_socket.connect((com_server_host, cd_com_port))
 
-dac_com_port = 32
+dac_com_port = 26
 dac_com_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dac_com_socket.connect((com_server_host, dac_com_port))
 
@@ -25,23 +25,22 @@ commandList = {
     "cd": {
         "power": "@POWER ON\r",
         "open": "@KEY 00\r",
+        "display": "@KEY 42\r",
+        "info": "@KEY 3C\r",
         "play": "@KEY 01\r",
+        "pause": "@KEY 02\r",
         "stop": "@KEY 03\r",
         "backward": "@KEY 0B\r",
-        "next": "@KEY 0C\r",
-        "mute": "@KEY 0D\r",
-        "pause": "@KEY 02\r",
-        "info": "@KEY 3C\r",
-        "display": "@KEY 42\r",
+        "forward": "@KEY 0C\r",
     },
     "dac": {
         "power": "@POWER ON\r",
+        "display": "@KEY 42\r",
+        "info": "@KEY 3C\r",
         "net": "@KEY 32\r",
         "line": "@KEY 38\r",
         "coaxial1": "@KEY 36\r",
         "coaxial2": "@KEY 37\r",
-        "info": "@KEY 3C\r",
-        "display": "@KEY 42\r",
         "hp63": "@AOUT TRS\r",
         "hpxlr4": "@AOUT XLR4 A\r",
         "xlr": "@AOUT XLR 2\r",
